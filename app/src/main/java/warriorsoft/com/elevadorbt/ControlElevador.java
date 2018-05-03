2eb928d08092d49c84c1e5654401dbca6ce837fe
Erick Guerrero <erickg@valles.udg.mx>
2018-05-03 09:31:07 -0500

    Initial commit

---
+++ app/src/main/java/warriorsoft/com/elevadorbt/ControlElevador.java
@@ -0,0 +1,138 @@
+package warriorsoft.com.elevadorbt;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ControlElevador extends AppCompatActivity implements View.OnClickListener{
+    Button btn1, btn2, btn3;
+    String direccion = null;
+    BluetoothAdapter myBluetooth;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConectado = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_elevador);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Intent intent = getIntent();
+        direccion = intent.getStringExtra("direccion");
+
+        btn1 = findViewById(R.id.btnPiso1);
+        btn2 = findViewById(R.id.btnPiso2);
+        btn3 = findViewById(R.id.btnPiso3);
+
+
+        new ConectarBT().execute();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                desconectar();
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnPiso1:
+                irAPiso("1");
+                break;
+            case R.id.btnPiso2:
+                irAPiso("2");
+                break;
+            case R.id.btnPiso3:
+                irAPiso("3");
+                break;
+        }
+    }
+
+
+    private class ConectarBT extends AsyncTask<Void, Void, Void>{
+        private boolean conexionExitosa = true;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getApplicationContext(),"Intentando conectar",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+          try{
+              if(btSocket == null || !isBtConectado){
+                  myBluetooth = BluetoothAdapter.getDefaultAdapter();
+                  BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(direccion);
+                  btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
+                  BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                  btSocket.connect();
+              }
+          }catch (IOException e){
+              conexionExitosa = false;
+          }
+          return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if(!conexionExitosa){
+                mensaje("Conexión fallida, Vuelve a intentar");
+            }else{
+                mensaje("Conectado");
+                isBtConectado = true;
+            }
+        }
+
+
+    }
+
+    private void mensaje(String msj){
+        Toast.makeText(getApplicationContext(),msj,Toast.LENGTH_LONG).show();
+    }
+
+    private void desconectar(){
+        if(btSocket!=null){
+            try{
+                btSocket.close();
+                mensaje("Desconectado");
+            }catch (IOException e){
+                mensaje("Error: "+e.getMessage());
+            }
+        }
+    }
+
+
+    private void irAPiso(String piso){
+        if(btSocket != null){
+            try{
+                btSocket.getOutputStream().write(piso.toString().getBytes());
+            }catch (IOException e){
+                mensaje("Error"+e.getMessage());
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/warriorsoft/com/elevadorbt/ListaDispositivos.java
@@ -0,0 +1,120 @@
+package warriorsoft.com.elevadorbt;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ListaDispositivos extends AppCompatActivity {
+    ListView listaDispositivos;
+    private BluetoothAdapter myBluetooth = null;
+    private Set dispositivosPairados;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_dispositivos);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        listaDispositivos = findViewById(R.id.lvDispositivos);
+
+
+
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if (myBluetooth==null){
+            Snackbar.make(listaDispositivos,"Esta cosa no tiene bluetooth",Snackbar.LENGTH_INDEFINITE).show();
+            finish();
+        }else{
+            if (!myBluetooth.isEnabled()){
+                Intent encenderBt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(encenderBt,1);
+            }
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                listaDeDispositivosConectados();
+
+                Snackbar.make(view, "Cargando dispositivos", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+
+
+    private void listaDeDispositivosConectados(){
+        dispositivosPairados = myBluetooth.getBondedDevices();
+        ArrayList lista = new ArrayList();
+
+        if(dispositivosPairados.size()>0){
+            for (Iterator<BluetoothDevice> bt = dispositivosPairados.iterator(); bt.hasNext();){
+                BluetoothDevice btD = bt.next();
+                lista.add(btD.getName() + "\n" + btD.getAddress());
+            }
+
+        }else{
+            Toast.makeText(getApplicationContext(),"No hay dispositivos conectados",Toast.LENGTH_LONG).show();
+        }
+
+        final ArrayAdapter adaptador = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,lista);
+        listaDispositivos.setAdapter(adaptador);
+        listaDispositivos.setOnItemClickListener(myListClickListener);
+
+    }
+
+
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String informacion = ((TextView) view).getText().toString();
+            String direccion = informacion.substring(informacion.length()-17);
+            Intent i = new Intent(ListaDispositivos.this,ControlElevador.class);
+            i.putExtra("direccion",direccion);
+            startActivity(i);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lista_dispositivos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
